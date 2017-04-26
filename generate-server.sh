92e03951cc3da7130737097d63f363a309db9753
DerEnderKeks <derenderkeks@gmail.com>
2017-04-26 18:25:01 +0200

    Added log output for generate-server.sh

--- generate-server.sh
+++ generate-server.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+# This script generates all config files and creates a default save
 
 mkdir -p $(dirname "$CONFIG")
+mkdir -p /srv/factorio/mods
 
-cp ./data/server-settings.example.json "$CONFIG"
-echo "[]" > "$WHITELIST"
-echo "[]" > "$BANLIST"
+if [ ! -f "$CONFIG" ]; then echo "Copying server config..."; cp ./data/server-settings.example.json "$CONFIG"; fi
+if [ ! -f "$WHITELIST" ]; then echo "Creating whitelist..."; echo "[]" > "$WHITELIST"; fi
+if [ ! -f "$BANLIST" ]; then echo "Creating blacklist..."; echo "[]" > "$BANLIST"; fi
 
-"$SERVER" --create ./saves/default.zip
+if [ ! -f "./saves/default.zip" ]; then echo "Creating default save..."; "$SERVER" --create ./saves/default.zip; fi
